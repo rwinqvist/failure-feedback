32236090a5c9205bb464079aad4de3d7ec0e173a
rwinqvist <rebwin@kth.se>
2024-08-07 11:40:10 +0200

    environment and experiment data

---
+++ py_setup/env_data.py
@@ -0,0 +1,86 @@
+ENV_INFO = {"env1": 
+                {   
+                    "length": 10,
+                    "terrains": ["A", "C", "R", "L", "P"],
+                    "severities": [1, 2, 3, 4, 5],
+                    "num_actions": 3,
+                    "nom_success_rate": 1/2,
+                    "action_cost_increment": 2,
+                    "risky_decline_factor": 0.7,
+                    "severity_decline_factor": 0.9,
+                    "skew_factor": 1,
+
+                    "A": {
+                        "type": "allowed",
+                        "p": 1,
+                    },
+
+                    "C": {
+                        "type": "allowed",
+                        "p": 0,
+                    },
+
+                    "R": {
+                        "type": "forbidden",
+                        "p": 1/3,
+                        "nom_sev_probs": [0.1, 0.15, 0.2, 0.25, 0.3],
+                    },
+
+                    "L": {
+                        "type": "forbidden",
+                        "p": 1/3,
+                        "nom_sev_probs": [0.25, 0.25, 0.2, 0.2, 0.1],
+                    },
+
+                    "P": {
+                        "type": "forbidden",
+                        "p": 1/3,
+                        "nom_sev_probs": [0.2, 0.2, 0.2, 0.2, 0.2],
+                    }
+                },
+
+
+                "env2": 
+                {   
+                    "length": 10,
+                    "terrains": ["A", "C", "R", "L", "P"],
+                    "severities": [1, 2, 3, 4, 5],
+                    "num_actions": 3,
+                    "nom_success_rate": 1/3,
+                    "action_cost_increment": 2,
+                    "risky_decline_factor": 0.7,
+                    "severity_decline_factor": 0.9,
+                    "skew_factor": 1,
+
+                    "A": {
+                        "type": "allowed",
+                        "p": 1,
+                    },
+
+                    "C": {
+                        "type": "allowed",
+                        "p": 0,
+                    },
+
+                    "R": {
+                        "type": "forbidden",
+                        "p": 1/3,
+                        "nom_sev_probs": [0.1, 0.15, 0.2, 0.25, 0.3],
+                    },
+
+                    "L": {
+                        "type": "forbidden",
+                        "p": 1/3,
+                        "nom_sev_probs": [0.25, 0.25, 0.2, 0.2, 0.1],
+                    },
+
+                    "P": {
+                        "type": "forbidden",
+                        "p": 1/3,
+                        "nom_sev_probs": [0.2, 0.2, 0.2, 0.2, 0.2],
+                    }
+                },
+
+
+
+}
