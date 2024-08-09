62102f6255d03c155ec673ea7ab126f3836e7e30
rwinqvist <rebwin@kth.se>
2024-08-09 14:35:30 +0200

    cache

--- py_setup/exp_data.py
+++ py_setup/exp_data.py
@@ -5,6 +5,24 @@ EXP_INFO = {"exp1":
                     "severity_penalty_weight": 0,
                     "recovery_rate": 0.2,
                     "halt_factor": 1
-                }
+                },
+
+            "exp2":
+                {    
+                    "true_obs_prob": 1/3,
+                    "query_cost": 1,
+                    "severity_penalty_weight": 0,
+                    "recovery_rate": 0.5,
+                    "halt_factor": 1
+                },
+            
+            "exp3":
+            {
+                "true_obs_prob": 0.2,
+                "query_cost": 1,
+                "severity_penalty_weight": 0,
+                "recovery_rate": 0.5,
+                "halt_factor": 1.2
+            }
 
 }
